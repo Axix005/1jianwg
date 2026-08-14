1a43355485de7fbccba007c7558960c7fc2a23d2
Axix005 <168464320+Axix005@users.noreply.github.com>
2026-08-14 10:42:55 +0800

    Update curl command with max-time option

--- wg.sh
+++ wg.sh
@@ -258,10 +258,10 @@ setup_config() {
     cd /etc/docker/containers/wg-easy || exit
 
     log_info "下载官方Docker Compose文件..."
-    if ! curl -L -o docker-compose.yml --connect-timeout 10 --retry 5 --retry-delay 3 https://raw.githubusercontent.com/wg-easy/wg-easy/master/docker-compose.yml; then
-	log_error "下载失败，请检查网络后重试，或手动下载该文件到 $(pwd)"
+if ! curl -L -o docker-compose.yml --connect-timeout 10 --max-time 30 --retry 5 --retry-delay 3 https://raw.githubusercontent.com/wg-easy/wg-easy/master/docker-compose.yml; then
+    log_error "下载失败，请检查网络后重试，或手动下载该文件到 $(pwd)"
     exit 1
-    fi
+fi
 
     # ==================== 禁用https ====================
     log_info "配置禁用https"
